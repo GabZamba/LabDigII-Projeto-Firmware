0ad9a9ac80162960c50e6dbfa999435bb639175c
PedroDeSanti <phmartinsanti@gmail.com>
2022-11-23 12:25:36 -0300

    🍻 Initial implementation
    Co-authored-by: jvtdegelo <jvtdegelo@users.noreply.github.com>
    Co-authored-by: Gabriel Zambelli <GabZamba@users.noreply.github.com>

---
+++ config.h
@@ -0,0 +1,21 @@
+/**
+ * @file config.h
+ *
+ * @author Pedro Henrique Martins de Santi <pedrodesanti@usp.br>
+ *
+ * @brief Configuration and definition for ESP8266 project
+ *
+ * @date 2022-03-30
+ *
+ * @copyright Copyright (c) 2022
+ *
+ */
+
+#ifndef CONFIG_H
+#define CONFIG_H
+
+#define RX_PIN 16
+#define TX_PIN 5
+
+#endif // CONFIG_H
+
